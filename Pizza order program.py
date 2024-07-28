4e667f8a07a3a7aff1180462fe00ab1dffe10ab4
SanjanaK <134064340+presslock@users.noreply.github.com>
2024-07-28 18:42:56 +0530

    Update Pizza order program.py

--- Pizza order program.py
+++ Pizza order program.py
@@ -1,26 +1,26 @@
 print("Thank you for choosing Python Pizza Deliveries!")
-print("Please make sure you don't have any spelling errors, spaces or wrong capital letters while giving your input!")
-size = input("Which size pizza do you want? Small, Medium, or Large?") 
-add_pepperoni = input("Do you want pepperoni in your pizza? Yes or No") 
-extra_cheese = input('Do you want extra cheese in your pizza? Yes or No') 
+print("Please make sure you don't have any spelling errors, spaces and write the letters in all CAPS!")
+size = input("Which size pizza do you want? S, M, or L?") 
+add_pepperoni = input("Do you want pepperoni in your pizza? Y or N") 
+extra_cheese = input('Do you want extra cheese in your pizza? Y or N') 
 
 bill = 0 
-if size == 'Small':
+if size == 'S':
   bill += 15
-elif size == "Medium":
+elif size == "M":
   bill += 20
-elif size == "Large":
+elif size == "L":
   bill += 25
 else:
   print("Error!! Please check the spelling or Cap letters or unwanted spaces.")
 
-if add_pepperoni == "Yes":
-  if size == "Small":
+if add_pepperoni == "Y":
+  if size == "S":
     bill += 2
-  elif size == "Medium" or "Large":
+  elif size == "M" or "L":
     bill += 3
 
-if extra_cheese == "Yes":
+if extra_cheese == "Y":
   bill += 1
 
 
